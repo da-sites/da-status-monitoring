39b827f89d003779dc2a0e71a1a2d8109e178974
David Bosschaert <bosschae@adobe.com>
2024-03-12 12:45:15 +0000

    Very basic ping test suite

---
+++ test/ping.test.js
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2024 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+import assert from 'assert';
+import jsdom from 'jsdom';
+
+const DA_ADMIN_HOST = process.env.DA_ADMIN_HOST || 'https://admin.da.live';
+const DA_COLLAB_HOST = process.env.DA_COLLAB_HOST || 'https://collab.da.live';
+const DA_LIVE_HOST = process.env.DA_LIVE_HOST || 'https://da.live'
+
+console.log('Environment configuration:');
+console.log('DA_ADMIN_HOST =', DA_ADMIN_HOST);
+console.log('DA_COLLAB_HOST =', DA_COLLAB_HOST);
+console.log('DA_LIVE_HOST =', DA_LIVE_HOST);
+
+describe('Ping Suite', () => {
+  it('Ping da-admin', async () => {
+    const res = await fetch(`${DA_ADMIN_HOST}/source/documentauthoring/da-aem-monitoring/tests/pingtest.html`)
+    const txt = await res.text();
+    assert(txt.includes('<p>ping</p>'), 'da-admin is down. Expected result not found in document: ' + txt);
+  });
+
+  it('Ping da-collab', async () => {
+    const res = await fetch(`${DA_COLLAB_HOST}/api/v1/ping`);
+    const json = await res.json();
+    assert.equal('ok', json.status, 'da-collab is down');
+  });
+
+  it('Ping da-live', async () => {
+    const res = await fetch(DA_LIVE_HOST);
+    const html = await res.text();
+
+    const dom = new jsdom.JSDOM(html);
+    assert.equal('Browse - Dark Alley', dom.window.document.querySelector("title").textContent,
+      'da-live is down. Home page not rendering');
+  })
+});
